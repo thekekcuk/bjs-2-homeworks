6cab406bf1b6dc817edd782ec6f64b6c017c1b16
Ilbakhtin Sergey <sonyk753@gmail.com>
2021-09-20 23:28:51 +0500

    Решение 1 задачи

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,31 @@
+'use strict'
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
-  return arr; // array
+  let d = b ** 2 - 4 * a * c;
+  if (d < 0) {
+    return arr;
+  }
+  if (d === 0) {
+    arr[0] = -b / (2 * a);
+    return arr;
+  }
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+    return arr;
+  }
+  return null;
 }
-
+//////////////////////////////////////////////////////////////////////////
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
 
   return totalAmount;
+}
+
+function fu(){
+  alert(123123123);
 }
