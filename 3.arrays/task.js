aabf5eb42de4eb09c6d47566f756785427596df1
Ilbakhtin Sergey <sonyk753@gmail.com>
2021-11-10 00:27:10 +0500

    Домашняя работа

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+'use strict'
+function compereArrays(arr1, arr2) {
+  let result = arr1.every(elem => arr1[elem] == arr2[elem]);
+  return result;
 }
+console.log (compereArrays([1, 2, 3], [1, 2, 3]));
+
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(elem => elem > 0).filter(elem => elem % 3).map(elem => elem * 10);
   return resultArr; // array
 }
+
+advancedFilter([1, 6, 3, 20, 30, -1, -2, -3])
