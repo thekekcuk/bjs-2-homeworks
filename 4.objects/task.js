a1c9878c89f6b9516ffe003de23d2238ee8c5eef
Ilbakhtin Sergey <sonyk753@gmail.com>
2021-11-24 23:09:13 +0500

    Итог

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -9,7 +9,7 @@
 //
 // // ваш код для остальных методов
 
-
+'use strict'
 function Student(name, gender, age) {
     // Ваш код
     //let year = new Date().getFullYear();
@@ -31,12 +31,17 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMark = function (mark) {
     let newProperty = 'marks';
-    this.marks = mark;
+    if ( this.marks === undefined) {
+        this.marks = [mark]; // Первая оценка добавляется в массив
+    } else {
+        this.marks.push(mark); // Все остальные оценки пушатся
+    }
 }
 
 Student.prototype.addMarks = function (...mark) {
     let newProperty = 'marks';
     this.marks = mark;
+
 }
 
 Student.prototype.getAverage = function () {
@@ -51,10 +56,15 @@ Student.prototype.exclude = function (reason) {
     // delete Student.prototype.subject;
     // delete Student.prototype.setSubject();
     // delete Student.prototype.setSubject;
-    delete Student.subject;
-    delete Student.prototype.addMarks;
+    delete this.subject;
+    delete this.marks;
 }
 // ваш код для остальных методов
-student22.setSubject('qwe');
-student22.exclude('удален');
+student22.addMark(5);
+student22.addMark(4);
+student22.addMark(3);
+student22.setSubject('Физика');
+student22.exclude('Леньтяй');
 console.log(student22);
+student3.addMarks(4,4,5);
+console.log(student3);
